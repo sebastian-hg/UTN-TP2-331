ee14b78504c159250eea58670f2477545798529e
sebastian hernandez <sebastianhg479@gmail.com>
2025-06-03 21:29:36 -0300

    se hacen cambios en el app .js , resuelve problema de cors

--- back/app.js
+++ back/app.js
@@ -1,46 +1,14 @@
-// const express = require("express");
-// const app = express();
-
-// const authRuta = require("../back/src/rutas/auth");
-// const productoRoutes = require("./src/rutas/producto")
-// //base de datod
-// const conexion = require("./src/config/database");
-
-// const { cargarDatosIniciales } = require("./script");
-
-// app.use(express.json());
-
-// // Rutas públicas de auth (login, registro)
-// app.use("/auth", authRuta);
-
-// // // Rutas de productos
-// app.use("/productos", productoRoutes);
-
-// app.listen(3000, () => {
-//   console.log("Servidor corriendo en http://localhost:3000");
-// });
-
-// app.use(express.json());
-
-// conexion
-//   .sync({ force: true })
-//   .then(async () => {
-//     console.log("Tablas sincronizadas");
-
-//     await cargarDatosIniciales();
-//   })
-//   .catch((error) => {
-//     console.error("Error al sincronizar la base de datos:", error);
-//   });
-
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+// Importar rutas
+const authRuta = require("./src/rutas/auth");
+const productoRoutes = require("./src/rutas/producto");
+const dashboardRoutes = require("./src/rutas/dashboard");
 const app = express();
 
-// 👇 CORS para permitir acceso global (si quieres restringir, configura origin)
 app.use(cors()); // Para permitir todas las solicitudes cross-origin
 
 // Configuración de EJS para vistas
@@ -54,17 +22,13 @@ app.use(express.json());
 // Middleware para parsear cookies
 app.use(cookieParser());
 
-// Importar rutas
-const authRuta = require("./src/rutas/auth");
-const productoRoutes = require("./src/rutas/producto");
-const dashboardRoutes = require("./src/rutas/dashboard");
 
 // Usar rutas
 app.use("/productos", productoRoutes);
 app.use("/auth", authRuta);
 app.use("/", dashboardRoutes); // Rutas protegidas y dashboard
 
-// Ruta raíz redirige a login
+// Ruta raíz redirige a login . momentaneamente
 app.get("/", (req, res) => {
   res.redirect("/auth/login");
 });
