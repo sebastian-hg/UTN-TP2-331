cadcba08875b6c9fc91c568994cb112ccf0fb31a
sebastian hernandez <sebastianhg479@gmail.com>
2025-06-16 13:22:43 -0300

    se agregan imagenes

--- back/script.js
+++ back/script.js
@@ -24,20 +24,20 @@ const cargarDatosIniciales = async () => {
 
     // 2. Crear productos
     const productos = await Producto.bulkCreate([
-      { nombre: "Camiseta Basica", categoria: "ropa", precio: 1500, imagen: "https://cdn.pixabay.com/photo/c/03/31/19/56/t-shirt-1296421_1280.png" },
-      { nombre: "Jeans Ajustados", categoria: "ropa", precio: 3500, imagen: "https://cdn.pixabay.com/photo/2016/03/31/19/56/jeans-1296397_1280.png" },
-      { nombre: "Chaqueta de Cuero", categoria: "ropa", precio: 7000, imagen: "https://cdn.pixabay.com/photo/2017/08/06/22/03/jacket-2592499_1280.jpg" },
-      { nombre: "Vestido Veraniego", categoria: "ropa", precio: 2800, imagen: "https://cdn.pixabay.com/photo/2017/07/16/10/43/dress-2503541_1280.jpg" },
-      { nombre: "Suéter de Lana", categoria: "ropa", precio: 3200, imagen: "https://cdn.pixabay.com/photo/2017/08/01/14/34/sweater-2566270_1280.jpg" },
-      { nombre: "Pantalones Deportivos", categoria: "ropa", precio: 2200, imagen: "https://cdn.pixabay.com/photo/2014/12/27/15/40/trousers-581554_1280.jpg" },
-      { nombre: "Camisa Formal", categoria: "ropa", precio: 4000, imagen: "https://cdn.pixabay.com/photo/2016/03/27/19/56/shirt-1286415_1280.png" },
-      { nombre: "Zapatos Deportivos", categoria: "calzado", precio: 4500, imagen: "https://cdn.pixabay.com/photo/2014/04/03/10/31/sneakers-309149_1280.png" },
-      { nombre: "Botas de Cuero", categoria: "calzado", precio: 8000, imagen: "https://cdn.pixabay.com/photo/2016/11/21/16/38/boots-1845299_1280.jpg" },
-      { nombre: "Sandalias Verano", categoria: "calzado", precio: 2500, imagen: "https://cdn.pixabay.com/photo/2017/05/10/20/28/sandals-2300124_1280.jpg" },
-      { nombre: "Zapatos Formales", categoria: "calzado", precio: 6000, imagen: "https://cdn.pixabay.com/photo/2014/12/15/14/04/shoes-568248_1280.jpg" },
-      { nombre: "Zapatillas Casual", categoria: "calzado", precio: 3800, imagen: "https://cdn.pixabay.com/photo/2017/01/25/15/43/shoes-2001690_1280.jpg" },
-      { nombre: "Mocasines", categoria: "calzado", precio: 5200, imagen: "https://cdn.pixabay.com/photo/2017/01/16/15/10/moccasin-1985971_1280.jpg" },
-      { nombre: "Chanclas Playa", categoria: "calzado", precio: 1200, imagen: "https://cdn.pixabay.com/photo/2017/01/16/15/11/flip-flops-1985977_1280.jpg" },
+      { nombre: "Camiseta Basica", categoria: "ropa", precio: 1500, imagen: "casmisaBlanca.jpeg"},
+      { nombre: "Jeans Ajustados", categoria: "ropa", precio: 3500, imagen: "jeansAjustados.jpeg" },
+      { nombre: "Chaqueta de Cuero", categoria: "ropa", precio: 7000, imagen: "chaquetaCuero.jpeg" },
+      { nombre: "Vestido Veraniego", categoria: "ropa", precio: 2800, imagen: "vestidoVeraniego.jpeg" },
+      { nombre: "Suéter de Lana", categoria: "ropa", precio: 3200, imagen: "sueterLana.jpeg" },
+      { nombre: "Pantalones Deportivos", categoria: "ropa", precio: 2200, imagen: "pantalonesDeportivos.jpeg" },
+      { nombre: "Camisa Formal", categoria: "ropa", precio: 4000, imagen: "camisaFormal.jpeg" },
+      { nombre: "Zapatos Deportivos", categoria: "calzado", precio: 4500, imagen: "zapatosDeportivos.jpeg" },
+      { nombre: "Botas de Cuero", categoria: "calzado", precio: 8000, imagen: "botasCuero.jpeg" },
+      { nombre: "Sandalias Verano", categoria: "calzado", precio: 2500, imagen: "sandaliasVerano.jpeg" },
+      { nombre: "Zapatos Formales", categoria: "calzado", precio: 6000, imagen: "zapatosFormales.jpeg" },
+      { nombre: "Zapatillas Casual", categoria: "calzado", precio: 3800, imagen: "zapatillasCasual.jpeg" },
+      { nombre: "Mocasines", categoria: "calzado", precio: 5200, imagen: "mocasines.jpeg" },
+      { nombre: "Chanclas Playa", categoria: "calzado", precio: 1200, imagen: "chanclasPlaya.jpeg" },
     ]);
 
     // 3. Asociar tallas según categoría
